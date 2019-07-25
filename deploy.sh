cdb02bb5116ad8eb87e5b4cc69038462ee69a3ff
Thiago Hersan <thiago.hersan+github@gmail.com>
2019-07-25 00:53:58 -0300

    adds 2019 submodule

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,14 @@ git pull origin master
 jekyll build --destination _site/2018
 popd
 
+## 2019
+pushd interactivos2019
+git pull origin master
+jekyll build --destination _site/2019
+popd
+
 ## update submodules
-git add interactivos2016 interactivos2018
+git add interactivos2016 interactivos2018 interactivos2019
 git commit -m "update submodules to their latest commit"
 git push origin master
 git checkout -- .
@@ -25,11 +31,13 @@ git checkout -- .
 git checkout --orphan gh-pages
 mv interactivos2016/_site/2016 .
 mv interactivos2018/_site/2018 .
+mv interactivos2019/_site/2019 .
 
 git rm --cached -r .
 git add CNAME index.html js
 git add 2016
 git add 2018
+git add 2019
 git commit -m "updates sites"
 git push origin :gh-pages
 git push -u origin gh-pages
